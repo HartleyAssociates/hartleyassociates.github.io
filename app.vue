c6f5913730fece3fc74ab5269243b2e5fbc9288a
Liam Mills <liam.m.mills@googlemail.com>
2024-05-04 13:06:36 +0100

    feat: add error page

--- app.vue
+++ app.vue
@@ -30,6 +30,30 @@ body,
   width: 100%;
 }
 
+.body-container,
+.main-container {
+  position: relative;
+  z-index: $zindex-zero;
+}
+
+.body-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: stretch;
+  position: relative;
+  z-index: $zindex-zero;
+  overflow: scroll;
+  overflow-x: hidden;
+  height: 100%;
+  scroll-behavior: smooth;
+  padding-top: px-to-rem(140px);
+
+  @media only screen and (min-width: 800px) {
+    padding-top: px-to-rem(122px);
+  }
+}
+
 .page-enter-active,
 .page-leave-active {
   transition: all 0.5s;
--- components/Container.vue
+++ components/Container.vue
@@ -8,29 +8,3 @@
     <FooterElement />
   </div>
 </template>
-
-<style lang="scss">
-.body-container,
-.main-container {
-  position: relative;
-  z-index: $zindex-zero;
-}
-
-.body-container {
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: stretch;
-  position: relative;
-  z-index: $zindex-zero;
-  overflow: scroll;
-  overflow-x: hidden;
-  height: 100%;
-  scroll-behavior: smooth;
-  padding-top: px-to-rem(140px);
-
-  @media only screen and (min-width: 800px) {
-    padding-top: px-to-rem(122px);
-  }
-}
-</style>
