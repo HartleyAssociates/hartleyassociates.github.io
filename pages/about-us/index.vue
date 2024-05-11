a5ffe66b20ecfc12fd773364147eb5d93f1b60c9
Liam Mills <liam.m.mills@googlemail.com>
2024-05-11 15:48:05 +0100

    fix: amend about-us LogoText text

--- pages/about-us/index.vue
+++ pages/about-us/index.vue
@@ -4,22 +4,10 @@
 
     <LogoText>
       <p>
-        <strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas.
-        Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
-        egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et
-        sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet,
-        wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac
-        dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a
-        est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est
-        malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a
-        est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est
-        malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.
+        Hartley Associates is a family run financial advice and planning firm. We have over 50 years of experience under
+        one roof. From investment and retirement planning to helping a young couple get that first step onto the housing
+        ladder. We pride ourselves on our client centric approach and successful results carefully tailored to the
+        clients needs.
       </p>
     </LogoText>
   </Container>
